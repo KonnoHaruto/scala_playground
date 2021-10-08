4eeaa3bb93ec7c3fb505587a2eff25cf6e5a81e0
Haruto Konno <qingdoujinye@gmail.com>
2021-10-08 20:15:24 +0900

    add Map

--- Factorization.scala
+++ Factorization.scala
@@ -4,5 +4,9 @@ object Factorization extends App {
   val target = 24
   val maxDivisor = sqrt(target).toInt
 
-  println(maxDivisor)
+  def factorizationReq(num: Int, divisor: Int, acc: Map[Int, Int]): Map[Int, Int] = {
+    Map(2 -> 3, 3 -> 1)
+  }
+
+  println(factorizationReq(target, 2, Map()))
 }
