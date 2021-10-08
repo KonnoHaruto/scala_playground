aba4719da192f8dd54de0fa5b102a0be8d53b33a
Haruto Konno <qingdoujinye@gmail.com>
2021-10-08 21:22:08 +0900

    finish

--- Factorization.scala
+++ Factorization.scala
@@ -1,17 +1,18 @@
 import scala.math.sqrt
 
 object Factorization extends App {
-  val target = 24
+  val target = 5010
   val maxDivisor = sqrt(target).toInt
 
   def factorizationReq( num: Int, divisor: Int, acc: Map[Int, Int] ): Map[ Int, Int ] = {
-    if ( divisor > maxDivisor ) {
-      if ( num == 1 ) acc else acc + ( num -> 1 )
-    } else if (num % divisor == 0) {
-      val nextAcc = acc + ( divisor -> ( acc.getOrElse( divisor, 0 ) + 1 ))
-      factorizationReq( num / divisor, divisor, nextAcc )
+    if (divisor > maxDivisor) {
+      if ( num == 1 ) acc else acc + (num -> 1)
+    } else if ( num % divisor == 0 ) {
+      //getOrElse関数は第一引数の値がもし無かったら第二引数の値とするということをやってくれる
+      val nextAcc = acc + ( divisor -> (acc.getOrElse(divisor, 0 ) + 1))
+      factorizationReq(num / divisor, divisor, nextAcc)
     } else {
-      factorizationReq( num, divisor + 1, acc )
+      factorizationReq(num, divisor + 1, acc)
     }
   }
 
